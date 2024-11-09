bd1cab817d8c8a44d42916ba9d3cabbc35088065
PAWAN KUMAR R <pavanyadav0071234@gmail.com>
2024-11-09 13:06:50 +0530

    Add files via upload

---
+++ str_len_with_loop.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[100]="hello";
+    int length=0;
+    while (str[length]!='\0')
+    {
+       length++; /* code */
+    }
+    printf("the length of the string is %d",length);
+    return 0;
+}
