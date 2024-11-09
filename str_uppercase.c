d6077c5321fb2960aa7f63e68428291bf88ff15d
PAWAN KUMAR R <pavanyadav0071234@gmail.com>
2024-11-09 12:53:10 +0530

    Add files via upload

---
+++ str_uppercase.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char a[50] = "hello";
+    char b[50];
+    char c[50] = "world";
+    strcpy(b, a);
+    printf("Copied string: %s\n", b);
+    printf("Length of string b: %d\n", strlen(b));
+    strcat(a, b);
+    printf("Concatenated string a: %s\n", a);
+    strcat(b, c);
+    printf("Concatenated string b: %s\n", b);
+    int res = strcmp(a, b);
+    printf("Comparison result: %d\n", res);
+    if (res == 0) {
+        printf("Both strings are equal\n");
+    } else if (res == -1) {
+        printf("String a is lesser than string b\n");
+    } else {
+        printf("String a is greater than string b\n");
+    }
+    printf("Reversed string a: %s\n", strrev(a));
+    for (int i = 0; a[i] != '\0'; i++) {
+        a[i] = toupper(a[i]);
+    }
+    printf("Uppercase string a: %s\n", a);
+    return 0;
+}
